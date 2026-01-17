2b5371545462f7b8ba9cd24c5007db55fb543572
Benjamin Menis <contacto.benjaminmenis@gmail.com>
2026-01-17 02:26:19 -0300

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,10 @@
+// Scroll suave
+document.querySelectorAll("a[href^='#']").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+console.log("Documental Web Qatar 2022 cargado");
